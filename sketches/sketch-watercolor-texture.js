269415e93a3fec4723cfe33855012cda4dd236c7
Pavel Yakimenko <yakimenkop@gmail.com>
2023-02-18 00:22:22 +0000

    Added sketch-watercolor-texture Minor refactoring in sketch-watercolor

--- sketches/sketch-watercolor-texture.js
+++ sketches/sketch-watercolor-texture.js
@@ -3,7 +3,7 @@ const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
 
 const settings = {
-  dimensions: [ 720, 720 ],
+  dimensions: [ 1080, 720 ],
   fps: 10,
   animate: true,
 };
@@ -17,9 +17,36 @@ let mainColor = `rgb(${mainColorComponents[0]}, ${mainColorComponents[1]}, ${mai
 // Secondary color
 let secondaryColorComponents = [random.range(0, 255), random.range(0, 255), random.range(0, 255)];
 let secondaryColor = `rgb(${secondaryColorComponents[0]}, ${secondaryColorComponents[1]}, ${secondaryColorComponents[2]}, ${brushOpacity})`;
-let enableDrawing = false;
-// Array of drawn objects with position, size, color and rotation
-const objects = [];
+let textures = [];
+
+// Function fill context with the texture
+function createTextureCanvas(baseCtx) {
+  let w = baseCtx.canvas.width;
+  let h = baseCtx.canvas.height;
+  // console.log(w * h / 500);
+  // Create a new canvas
+  const textureCanvas = document.createElement('canvas');
+  textureCanvas.width = w * 1.1;
+  textureCanvas.height = h * 1.1;
+  const textureContext = textureCanvas.getContext('2d');
+  
+  // Create array of random objects
+  let objects = [];
+  for (let i = 0; i < w * h / 500; i++) {
+    let x = random.range(0, w);
+    let y = random.range(0, h);
+    objects.push(createBrushSpot(x, y));
+  }
+  // Fill in the canvas with objects
+  objects.forEach((object, index) => {
+    let color = object.colors[0];
+    if (index > objects.length * 4 / 5) {
+      color = object.colors[1];
+    }
+    drawBrushSpot(textureContext, object, color);
+  });
+  return textureCanvas;
+}
 
 // Function draw a circle filled with the texture on a given context
 function drawTexturedCircle(baseCtx) {
@@ -56,35 +83,19 @@ function drawTexturedCircle(baseCtx) {
   baseCtx.fill();
 }
 
-// On mouse click, get position relative to context
-document.addEventListener('click', (e) => {
-  const rect = ctx.canvas.getBoundingClientRect();
-  let x = e.clientX - rect.left;
-  let y = e.clientY - rect.top;
-  // If click is inside color picker, randomize mainColor
-  if (x > 10 && x < 60 && y > 10 && y < 60) {
-    mainColorComponents = [random.range(0, 255), random.range(0, 255), random.range(0, 255)];
-    mainColor = `rgb(${mainColorComponents[0]}, ${mainColorComponents[1]}, ${mainColorComponents[2]}, ${brushOpacity})`;
-    secondaryColorComponents = [random.range(0, 255), random.range(0, 255), random.range(0, 255)];
-    secondaryColor = `rgb(${secondaryColorComponents[0]}, ${secondaryColorComponents[1]}, ${secondaryColorComponents[2]}, ${brushOpacity})`;
-  }
-});
-
 // Randomize mainColor
 function randomizeColor(colorComponents) {
   let extraColorComponents = [random.range(-25, 25), random.range(-25, 25), random.range(-25, 25)];
   return `rgb(${colorComponents[0] + extraColorComponents[0]}, ${colorComponents[1] + extraColorComponents[1]}, ${colorComponents[2] + extraColorComponents[2]}, ${brushOpacity})`;
 };
 
-// Get mouse position on mousemove relative to canvas
-window.addEventListener('mousemove', (event) => {
-  if (event.buttons != 1) return; // Ignore if no button pressed
-
-  const rect = ctx.canvas.getBoundingClientRect();
-  let x = event.clientX - rect.left;
-  let y = event.clientY - rect.top;
-  objects.push(createBrushSpot(x, y));
-});
+// Change main and secondary colors
+function changeColors() {
+  mainColorComponents = [random.range(0, 255), random.range(0, 255), random.range(0, 255)];
+  mainColor = `rgb(${mainColorComponents[0]}, ${mainColorComponents[1]}, ${mainColorComponents[2]}, ${brushOpacity})`;
+  secondaryColorComponents = [random.range(0, 255), random.range(0, 255), random.range(0, 255)];
+  secondaryColor = `rgb(${secondaryColorComponents[0]}, ${secondaryColorComponents[1]}, ${secondaryColorComponents[2]}, ${brushOpacity})`;
+}
 
 // Add object to array at coordinates
 function createBrushSpot(x, y) {
@@ -97,26 +108,33 @@ function createBrushSpot(x, y) {
   };
 }
 
+let opacity = 0.0;
+
 const sketch = ({ context, width, height }) => {
   ctx = context;
-  return ({ context, width, height }) => {
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, width, height);
-    ctx.fillStyle = mainColor;
-    ctx.fillRect(10, 10, 60, 60);
+  textures.push(createTextureCanvas(ctx), createTextureCanvas(ctx));
 
-    if (enableDrawing) {
-      objects.forEach ((object) => {
-        drawBrushSpot(ctx, object);
-      });
+  return ({ context, width, height }) => {
+    ctx.fillStyle = ctx.createPattern(textures[0], 'no-repeat');
+    ctx.fillRect(0, 0, width, height);
+    opacity += 0.1;
+    ctx.save();
+    ctx.globalAlpha = opacity;
+    ctx.fillStyle = ctx.createPattern(textures[1], 'no-repeat');
+    ctx.fillRect(0, 0, width, height);
+    ctx.restore();
+    if (opacity >= 1) {
+      opacity = 0;
+      textures.shift();
+      changeColors();
+      textures.push(createTextureCanvas(ctx));
     }
-    drawTexturedCircle(ctx);
   };
 };
 
 canvasSketch(sketch, settings);
 
-function drawBrushSpot(ctx, object) {
+function drawBrushSpot(ctx, object, color) {
   // It draws a circle, plotting the vertices on circumference
   ctx.save();
   ctx.translate(object.x, object.y);
@@ -136,7 +154,7 @@ function drawBrushSpot(ctx, object) {
     }
   }
   ctx.closePath();
-  ctx.fillStyle = object.colors[0];
+  ctx.fillStyle = color;
   ctx.strokeStyle = 'black';
   ctx.fill();
   ctx.restore();
